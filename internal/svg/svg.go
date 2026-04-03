19bd9a1baa34a91d79f11f65867ef856b8a966a9
Eli Burch <eli.burch@burchbytes.com>
2026-04-02 18:16:56 -0700

    Rasterize to RGBA, then copy to NRGBA

--- internal/svg/svg.go
+++ internal/svg/svg.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/draw"
 	"io"
 	"path/filepath"
 	"strconv"
@@ -80,7 +81,7 @@ func (d *Decoder) Draw(width, height int) (*image.NRGBA, error) {
 	x, y := svgOffset(d.icon, imgW, imgH)
 	d.icon.SetTarget(x, y, float64(imgW), float64(imgH))
 
-	img := image.NewNRGBA(image.Rect(0, 0, imgW, imgH))
+	img := image.NewRGBA(image.Rect(0, 0, imgW, imgH))
 	scanner := rasterx.NewScannerGV(config.Width, config.Height, img, img.Bounds())
 	raster := rasterx.NewDasher(width, height, scanner)
 
@@ -89,7 +90,10 @@ func (d *Decoder) Draw(width, height int) (*image.NRGBA, error) {
 		err = fmt.Errorf("SVG render error: %w", err)
 		return nil, err
 	}
-	return img, nil
+	bounds := img.Bounds()
+	out := image.NewNRGBA(bounds)
+	draw.Draw(out, bounds, img, bounds.Min, draw.Src)
+	return out, nil
 }
 
 func IsFileSVG(path string) bool {
