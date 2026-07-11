5bdcec45f6604306c91ed3eaa46c4fe313fd0f74
Tilo Prütz <tilo@pruetz.net>
2026-07-11 20:05:15 +0200

    [internal/painter/gl] remove superfluous mobile painter#glctx()

--- internal/painter/gl/gl_gomobile.go
+++ internal/painter/gl/gl_gomobile.go
@@ -65,17 +65,14 @@ var (
 	textureFilterToGL = [...]int32{gl.Linear, gl.Nearest, gl.Linear}
 )
 
-func (p *painter) glctx() gl.Context {
-	return p.contextProvider.Context().(gl.Context)
-}
-
 func (p *painter) Init() {
-	p.ctx = &mobileContext{glContext: p.contextProvider.Context().(gl.Context)}
+	glctx := p.contextProvider.Context().(gl.Context)
+	glctx.Disable(gl.DepthTest)
+	glctx.Enable(gl.Blend)
+	p.ctx = &mobileContext{glContext: glctx}
 	p.maxTextureSize = p.ctx.GetInteger(maxTextureSizeParam)
 	p.blurSnap.texValid = false   // reset on context recreation; old texture IDs are no longer valid
 	p.blurKernel.texValid = false // kernel texture must also be re-created
-	p.glctx().Disable(gl.DepthTest)
-	p.glctx().Enable(gl.Blend)
 	if compiled == nil {
 		compiled = &programs{
 			arbitraryPolygon: programState{
